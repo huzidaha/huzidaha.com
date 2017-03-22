6256c26ffdbe9dce3de38f7fdac71c3db566b51b
huzidaha <huzidaha@126.com>
2017-03-22 16:13:48 +0800

    change host

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 export const DEV = process.env.NODE_ENV !== 'production'
-export const BACKEND_URL = DEV ? 'http://localhost:3000' : 'http://huzidaha.com'
+export const BACKEND_URL = DEV ? 'http://localhost:3000' : 'http://blog.huziketang.com'
